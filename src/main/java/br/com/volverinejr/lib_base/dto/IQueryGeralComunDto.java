ace06be5f717ddaf94e6a2146ac6712f3eb3f321
volverinejr <volverinejr@gmail.com>
2022-01-18 13:27:36 -0300

    start

---
+++ src/main/java/br/com/volverinejr/lib_base/dto/IQueryGeralComunDto.java
@@ -0,0 +1,9 @@
+package br.com.volverinejr.lib_base.dto;
+
+public interface IQueryGeralComunDto {
+	Long getId();
+
+	String getNome();
+
+	String getCadastrado();
+}
---
+++ src/main/java/br/com/volverinejr/lib_base/dto/LogPesquisaDto.java
@@ -0,0 +1,36 @@
+package br.com.volverinejr.lib_base.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogPesquisaDto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@NotNull
+	private String servico;
+
+	@NotNull
+	private String usuario;
+
+	@NotNull
+	private String className;
+
+	@NotNull
+	private String metodo;
+
+	@NotNull
+	private String argumento;
+
+	@NotNull
+	private String resultado;
+}
