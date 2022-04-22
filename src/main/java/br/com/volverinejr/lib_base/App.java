f280a909bd51b8c79d08f4fb38b66da5b36fac58
volverinejr <volverinejr@gmail.com>
2022-04-22 10:05:40 -0300

    start

--- src/main/java/br/com/volverinejr/lib_base/App.java
+++ src/main/java/br/com/volverinejr/lib_base/App.java
@@ -4,6 +4,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/*
+ * File > New > Outros > Maven Project
+ * maven-central 
+ * filtro: maven-archetype-quickstart
+ * Configurar Build Path, pasta resources 
+ * */
+
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class App {
